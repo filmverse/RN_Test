b359fff79e07ffd2b858dcc88305a9db38d0ced3
Vikas Sharma <vkassharma19492@gmail.com>
2023-08-20 03:37:40 +0530

    Setting Up

--- TestPro2/components/NewApp.js
+++ TestPro2/components/NewApp.js
@@ -1,78 +1,34 @@
-import React, {useEffect, useState} from 'react';
-import {
-  FlatList,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  Alert,
-} from 'react-native';
+import React from "react";
+import { SafeAreaView, View, FlatList, Pressable, Text, TextInput, StatusBar, StyleSheet, Alert } from "react-native";
 
 const DATA = [
   {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
+    id: '1',
+    title: 'One',
   },
   {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
+    id: '2',
+    title: 'Two',
   },
   {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
+    id: '3',
+    title: 'Three',
   },
 ];
 
-const Item = ({item, onPress, backgroundColor, textColor}) => (
-  <TouchableOpacity onPress={onPress} style={[styles.item, {backgroundColor}]}>
-    <Text style={[styles.title, {color: textColor}]}>{item.title}</Text>
-  </TouchableOpacity>
+const Item = ({title}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
 );
 
 const NewApp = () => {
-  const [selectedId, setSelectedId] = useState();
-
-  const renderItem = ({item}) => {
-    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
-    const color = item.id === selectedId ? 'white' : 'black';
-
-    return (
-      <Item
-        item={item}
-        onPress={() => {
-            setSelectedId(item.id)
-            Alert.alert(item.id)
-        }}
-        backgroundColor={backgroundColor}
-        textColor={color}
-      />
-    );
-  };
-
-  useEffect(() => {
-    const delay = 1000; // 1 second delay for each item press
-
-    const simulatePressForItems = () => {
-      DATA.forEach((item, index) => {
-        setTimeout(() => {
-          setSelectedId(item.id);
-        //   Item({item: item});
-        }, index * delay);
-      });
-    };
-
-    simulatePressForItems();
-  }, []);
-
   return (
     <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
-        extraData={selectedId}
-      />
+      <View style={styles.container}>
+        <Text style={styles.title}>Flat List Example</Text>
+        <FlatList data={DATA} renderItem={({item}) => <Item title={item.title} />} keyExtractor={item => item.title} />
+      </View>
     </SafeAreaView>
   );
 };
@@ -80,16 +36,20 @@ const NewApp = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
     marginTop: StatusBar.currentHeight || 0,
   },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+    fontSize: 32,
+  },
   item: {
+    backgroundColor: '#f9c2ff',
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
   },
-  title: {
-    fontSize: 32,
-  },
-});
+})
 
 export default NewApp;
