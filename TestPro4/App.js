4a38624a2f2d6613029738385c4da0d4396bd551
Vikas Sharma <vkassharma19492@gmail.com>
2023-08-24 02:09:31 +0530

    finishing up

---
+++ TestPro4/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, SafeAreaView, TouchableOpacity, Text, FlatList, StyleSheet } from "react-native";
+import TestRenderer from 'react-test-renderer';
+
+const App = () => {};
+
+
+export default App;
