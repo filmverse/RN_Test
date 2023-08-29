47b4c58b7804fec213e72115991782f19ddb47fd
Vikas Sharma <vkassharma19492@gmail.com>
2023-08-30 03:12:59 +0530

    setting up

--- TestPro5/src/screens/Home.js
+++ TestPro5/src/screens/Home.js
@@ -1,13 +1,17 @@
-import { Button, Text, View, StyleSheet } from 'react-native'
+import { Button, Text, View, StyleSheet, Alert } from 'react-native'
 import React from 'react'
+import TestRenderer from 'react-test-renderer';
 
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text style={styles.textColor}>Home Screen</Text>
+      <Button title='Button1' onPress={() => {Alert.alert('Passing as Props')}} />
       <Button
         title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
+        onPress={() => navigation.navigate('Details', {
+          testProp: testProp,
+        })}
       />
     </View>
   );
@@ -18,5 +22,11 @@ const styles = StyleSheet.create({
     color: "#000000",
   }
 });
+
+const tree = TestRenderer.create(<HomeScreen />);
+const root = tree.root;
+
+const testProp = root.findAllByType(Button).map(ftt => ftt._fiber.child.pendingProps.onPress)
+console.log(testProp[1]);
 
 export default HomeScreen;
