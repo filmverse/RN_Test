4e5b853f88a6444bb5e8ead445ee08cac9c8f813
Vikas Sharma <vkassharma19492@gmail.com>
2023-09-12 14:54:43 +0530

    finishing up

--- TestPro9/App.js
+++ TestPro9/App.js
@@ -6,26 +6,33 @@ import {
   SafeAreaView,
   ScrollView,
   StatusBar,
+  Alert,
 } from 'react-native';
+import TestRenderer from 'react-test-renderer';
 
 const App = () => {
+  React.useEffect(() => {
+    if (flatTestRenderList) {
+      flatTestRenderList()
+    }
+  }, []);
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.text}>Payment App</Text>
       <Text style={styles.text}>Test App</Text>
       <ScrollView style={styles.scrollView}>
         <Text style={styles.text1}>Choose your payment method</Text>
-        <Button title='Credit Card' />
-        <Button title='Debit Card' />
-        <Button title='PayME' />
-        <Button title='BlazorPay' />
-        <Button title='IndiaPa' />
-        <Button title='MPay' />
-        <Button title='PhoneDe' />
-        <Button title='VPI' />
-        <Button title='Internet Banking' />
-        <Button title='Mobile Banking' />
-        <Button title='Other' />
+        <Button title='Credit Card' onPress={() => Alert.alert("Credit Card")} />
+        <Button title='Debit Card' onPress={() => Alert.alert("Debit Card")} />
+        <Button title='PayME' onPress={() => Alert.alert("PatME")} />
+        <Button title='BlazorPay' onPress={() => Alert.alert("BlazorPay")} />
+        <Button title='IndiaPa' onPress={() => Alert.alert("IndiaPa")} />
+        <Button title='MPay' onPress={() => Alert.alert("MPay")} />
+        <Button title='PhoneDe' onPress={() => Alert.alert("PhoneDe")} />
+        <Button title='VPI' onPress={() => Alert.alert("VPI")} />
+        <Button title='Internet Banking' onPress={() => Alert.alert("Internet Banking")} />
+        <Button title='Mobile Banking' onPress={() => Alert.alert("Mobile Banking")} />
+        <Button title='Other' onPress={() => Alert.alert("Other Payment Method")} />
         <Text style={styles.text1}>We are always their for you.</Text>
       </ScrollView>
       <Text style={styles.text}>Pay safely with us.</Text>
@@ -51,5 +58,14 @@ const styles = StyleSheet.create({
     fontSize: 30,
   },
 });
+
+const tree = TestRenderer.create(<App />);
+const root = tree.root;
+
+const flatTest = root.findAllByType(Button).map(ftt => ftt._fiber.child.pendingProps)
+const flatTestRender = flatTest.map(fttr => fttr.onPress)
+const flatTestRenderList = flatTestRender[1]
+
+console.log(flatTestRenderList, 'last log');
 
 export default App;
